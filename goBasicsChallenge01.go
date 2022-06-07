cdc67d2032562c8b5011d36e192345454d526de5
Bidyadhar Pal <manas5179@users.noreply.github.com>
2022-06-07 07:07:30 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,14 @@
+/* Alta3 Research | RZFeeser
+   Variables - Package and function level   */
+
+package main
+  
+import "fmt"
+
+
+func main() {
+        var city, state =  "Bangalore", "Karanataka"
+        fmt.Println("My name is Bidyadhar Pal")
+        fmt.Println("My city name is: ", city)
+        fmt.Println("My State name is: ", state)
+}
